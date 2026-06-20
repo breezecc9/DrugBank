5d0a08f130c8f08d9a4473dd9e66956fd62333aa
1nUrHrt <750725830@qq.com>
2026-06-20 19:47:48 +0800

    fix bug

--- pretty_printer.py
+++ pretty_printer.py
@@ -1,6 +1,9 @@
+import os
 import re
 from abc import ABC, abstractmethod
-from typing import Generic, TypeAlias, TypeVar
+from typing import Any, Generic, Optional, TypeVar
+
+from custom_printer import ptr_color
 
 
 class PtrColor:
@@ -45,9 +48,9 @@ class BaseValue(ABC, Generic[T]):
 
 
 class StringValue(BaseValue[str]):
-    def __init__(self) -> None:
+    def __init__(self, value: str = "") -> None:
         super().__init__()
-        self._value = ""
+        self._value = value
 
     def write(self, value):
         self._value = value
@@ -84,7 +87,7 @@ class PlaceholderValue(BaseValue[dict[str, str]]):
         return self._value
 
 
-class BaseValueTemplate(ABC, Generic[T]):
+class BaseValueTemplate(Generic[T]):
     def __init__(
         self, value: BaseValue[T], color: str = PtrColor.info, length: int = 30
     ) -> None:
@@ -104,8 +107,8 @@ class BaseValueTemplate(ABC, Generic[T]):
     def write(
         self,
         value: T,
-        color: str | None = None,
-        length: int | None = None,
+        color: Optional[str] = None,
+        length: Optional[int] = None,
     ):
         if color is not None:
             self.set_color(color)
@@ -114,10 +117,8 @@ class BaseValueTemplate(ABC, Generic[T]):
         self.set_value(value)
 
     def assemble(self) -> str:
-        template = "{lpad}{prefix}{value}{suffix}{rpad}"
+        template = f"{{lpad}}{self._color}{{value}}\033[0m{{rpad}}"
         template_dict = {"lpad": "", "rpad": ""}
-        template_dict["prefix"] = self._color
-        template_dict["suffix"] = PtrColor.info
         diff = self._length - len(self._value_template.value)
         if diff < 0:
             keep_len = max(self._length - 3, 0)
@@ -131,8 +132,8 @@ class BaseValueTemplate(ABC, Generic[T]):
     def w_assemble(
         self,
         value: T,
-        color: str | None = None,
-        length: int | None = None,
+        color: Optional[str] = None,
+        length: Optional[int] = None,
     ) -> str:
         self.write(value, color, length)
         return self.assemble()
@@ -141,35 +142,179 @@ class BaseValueTemplate(ABC, Generic[T]):
         return self._length
 
 
-value_template_index = {0: StringValue, 1: PlaceholderValue}
+class ScrollTemplate:
+    def __init__(self) -> None:
+        self.pos = 0
+        self._value_index_list: list[int] = []
+        self.length = 0
+
+    def add_template(self, index: int):
+        self._value_index_list.append(index)
+        self.length += 1
 
 
 class PrettyPrinter:
     def __init__(self) -> None:
-        self._tempalte = ""
-        self._template_dict: dict[str, BaseValueTemplate] = {}
+        self._template: list[BaseValueTemplate[Any]] = []
+        self._template_dict: dict[str, BaseValueTemplate[Any]] = {}
+        self._scroll_dict: dict[str, ScrollTemplate] = {}
 
-    def add_template(self, template: str):
-        placeholders = get_all_placeholder(template)
-        for placeholder in placeholders:
-            arr = placeholder.split(":")
-            if len(arr) < 4:
-                raise ValueError(f"{placeholder} error")
-            self._template_dict[arr[0]] = BaseValueTemplate(
-                StringValue(), color=arr[1], length=int(arr[2])
-            )
-            template.replace(placeholder, arr[0])
-        self._tempalte += template
+    def add_template(
+        self,
+        key: Optional[str],
+        template: BaseValue[T],
+        color: str = PtrColor.info,
+        length: int = 30,
+    ):
+        wrapper = BaseValueTemplate(template, color, length)
+        self._template.append(wrapper)
+        if key is not None:
+            self._template_dict[key] = wrapper
+        return self
+
+    def add_string_template(
+        self,
+        value: str = "",
+        color: str = PtrColor.info,
+        length: int = 30,
+        key: Optional[str] = None,
+    ):
+        return self.add_template(key, StringValue(value), color, length)
+
+    def next_line(
+        self,
+    ):
+        return self.add_template(None, StringValue("\n"), ptr_color.info, 2)
+
+    def split_line(self, length: int = 150, char: str = "-", front=True, back=True):
+        if front:
+            self.next_line()
+        self.add_template(
+            None, StringValue(char * length), ptr_color.info, length * len(char)
+        )
+        if back:
+            self.next_line()
+        return self
+
+    def add_placeholder_template(
+        self,
+        placeholders: str,
+        value: Optional[dict[str, str]] = None,
+        color: str = PtrColor.info,
+        length: int = 30,
+        key: Optional[str] = None,
+    ):
+        template = PlaceholderValue(placeholders)
+        if value is not None:
+            template.write(value)
+        return self.add_template(key, template, color, length)
+
+    def write(
+        self,
+        key: str,
+        value: str | dict[str, str],
+        color: Optional[str] = None,
+        length: Optional[int] = None,
+    ):
+
+        tpl = self._template_dict.get(key)
+        if tpl is None:
+            raise KeyError(f"不存在key={key}的模板")
+        tpl.write(value, color, length)
+
+    def w_flush(
+        self,
+        key: str,
+        value: str | dict[str, str],
+        color: Optional[str] = None,
+        length: Optional[int] = None,
+    ):
+        self.write(key, value, color, length)
+        self.flush()
 
     def flush(self):
-        flush_dict = {}
-        for k, v in self._template_dict.items():
-            flush_dict[k] = v.assemble()
+        self.clear_screen()
+        print("".join([v.assemble() for v in self._template]))
+
+    def clear_screen(self):
+        os.system("cls" if os.name == "nt" else "clear")
+
+    def add_scroll(self, key: str):
+        current_scroll = self._scroll_dict.get(key)
+        if current_scroll is None:
+            current_scroll = ScrollTemplate()
+            self._scroll_dict[key] = current_scroll
+        current_scroll.add_template(len(self._template) - 1)
+        return self
+
+    def scroll(
+        self,
+        key: str,
+        value: str | dict[str, str],
+        color: str | None = None,
+        length: int | None = None,
+    ):
+        current_scroll = self._scroll_dict.get(key)
+        if current_scroll is None:
+            return
+        if current_scroll.length <= 0:
+            return
+        if current_scroll.pos < current_scroll.length:
+            self._template[current_scroll._value_index_list[current_scroll.pos]].write(
+                value, color, length
+            )
+            current_scroll.pos += 1
+        else:
+            template_list: list[BaseValueTemplate[Any]] = []
+            for i in current_scroll._value_index_list:
+                template_list.append(self._template[i])
+            current_template = template_list.pop(0)
+            current_template.write(value, color, length)
+            template_list.append(current_template)
+            for i, j in enumerate(current_scroll._value_index_list):
+                self._template[j] = template_list[i]
+
+    def scl_flush(
+        self,
+        key: str,
+        value: str | dict[str, str],
+        color: str | None = None,
+        length: int | None = None,
+    ):
+        self.scroll(key, value, color, length)
+        self.flush()
 
 
-x = "{op:xx:12}{op1::12}"
-pattern = re.compile(r"\{(.+?)\}")
-placeholders = pattern.findall(x)
-for placeholder in placeholders:
-    print(placeholder.split(":"))
+pt_printer = PrettyPrinter()
+pt_printer.split_line().add_placeholder_template(
+    "Name:{value}", key="name"
+).add_placeholder_template("Encoder:{value}", key="encoder").add_placeholder_template(
+    "Classifier:{value}", key="classifier"
+).add_placeholder_template("LR:{value}", key="lr").add_placeholder_template(
+    "Device:{value}", key="device"
+).split_line().add_placeholder_template(
+    "Epoch:{value}", key="epoch"
+).add_placeholder_template("Elapsed:{value}", key="elapsed").add_placeholder_template(
+    "Early Stop:{value}", key="early_stop"
+).add_placeholder_template("State:{value}", key="state").add_placeholder_template(
+    "Stage:{value}", key="stage"
+).split_line().add_string_template("Train:").add_placeholder_template(
+    "Loss:{loss} Acc:{acc}", length=120, key="train"
+).split_line().add_string_template("Last Val:").add_placeholder_template(
+    "Loss:{loss} Acc:{acc} F1:{f1} Prec:{prec} Rec:{rec}", length=120, key="val"
+).split_line().add_string_template("Test:").add_placeholder_template(
+    "Loss:{loss} Acc:{acc} F1:{f1} Prec:{prec} Rec:{rec}", length=120, key="test"
+).split_line().add_string_template().add_string_template(length=120).add_scroll(
+    "info"
+).next_line().add_string_template().add_string_template(length=120).add_scroll(
+    "info"
+).next_line().add_string_template("Info").add_string_template(length=120).add_scroll(
+    "info"
+).next_line().add_string_template().add_string_template(length=120).add_scroll(
+    "info"
+).next_line().add_string_template().add_string_template(length=120).add_scroll(
+    "info"
+).split_line()
 
+
+__all__ = ["pt_printer", "PtrColor"]
